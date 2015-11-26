391b030a41bcca8a0940d79b6185cb5ffea08ff5
StuntRR <josh.crx919@gmail.com>
2015-11-26 14:30:27 -0600

    Fix thermal merge error

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -608,21 +608,6 @@ static int cpufreq_parse_dt(struct device *dev)
 	return 0;
 }
 
-#ifdef CONFIG_CPU_VOLTAGE_TABLE
-bool is_used_by_scaling(unsigned int freq)
-{
-	unsigned int i, cpu_freq;
-
-	for (i = 0; dts_freq_table[i].frequency != CPUFREQ_TABLE_END; i++) {
-		cpu_freq = dts_freq_table[i].frequency;
-		if (cpu_freq == CPUFREQ_ENTRY_INVALID)
-			continue;
-		if (freq == cpu_freq)
-			return true;
-	}
-	return false;
-}
-
 #ifdef CONFIG_DEBUG_FS
 static int msm_cpufreq_show(struct seq_file *m, void *unused)
 {
