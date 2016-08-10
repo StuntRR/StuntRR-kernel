6dade9b1869a1568affdabe509ea1d6e9e251497
StuntRR <josh.crx919@gmail.com>
2016-08-10 16:57:32 -0500

    Remove powersuspend
    I don't see it as useful anymore, so lets just remove it!

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -10,11 +10,6 @@
 #include <linux/mutex.h>
 #include <linux/pm_wakeup.h>
 
-#ifdef CONFIG_POWERSUSPEND
-#include <linux/powersuspend.h>
-#include <linux/syscalls.h>
-#endif
-
 #include "power.h"
 
 static suspend_state_t autosleep_state;
